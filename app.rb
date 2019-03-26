4bb4965350a04cee6ec635355b20f7e3d4bdda13
Clem Baldy <clemoun@yahoo.fr>
2019-03-26 11:18:32 +0100

    comments update in app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-# lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;)
+# lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
 require 'bundler'
 Bundler.require
 
@@ -8,5 +8,5 @@ require_relative 'lib/user'
 require_relative 'lib/event'
 
 
-# Open bar pour tester ton application. Tous les fichiers importants sont chargés
-# Tu peux faire User.new, Event.new, binding.pry, User.all, etc etc
+# Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
+# Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
